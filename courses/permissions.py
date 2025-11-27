c3bf2d0984a5ce27214cc830037f38493b791818
Benyamin Abdolseyedi <Bentako66@gmail.com>
2025-11-27 14:52:44 -0800

    feath:add IsAdminUser permission class

---
+++ courses/permissions.py
@@ -0,0 +1,5 @@
+from rest_framework.permissions import BasePermission, IsAuthenticated
+
+class IsAdminUser(IsAuthenticated):
+    def has_permission(self, request, view):
+        return super().has_permission(request, view) and request.user.is_admin()
