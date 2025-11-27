3b5f15218af57d519239f51fc846878210375aea
Benyamin Abdolseyedi <Bentako66@gmail.com>
2025-11-27 15:08:05 -0800

    create superuser and set role to ADMIN

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User  
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('نقش و دسترسی‌ها', {'fields': ('role',)}),
+    )
+    
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('نقش و دسترسی‌ها', {'fields': ('role',)}),
+    )
