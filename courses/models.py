e8d18c2388158da675b4c24a8ecef5bf0d7c17df
BaharTaheripour <bahartp82@gmail.com>
2025-12-04 23:55:19 +0330

    feat: make course fields optional (professor, day, time, location)

--- courses/models.py
+++ courses/models.py
@@ -5,12 +5,11 @@ class Course(models.Model):
     name = models.CharField(max_length=200)
     code = models.CharField(max_length=20, unique=True)
     capacity = models.PositiveIntegerField(default=0)
-    professor = models.CharField(max_length=150)
-    day = models.CharField(max_length=100)
-    time = models.CharField(max_length=20)
-    location = models.CharField(max_length=100)
+    professor = models.CharField(max_length=150, blank=True)
+    day = models.CharField(max_length=100, blank=True)
+    time = models.CharField(max_length=20, blank=True)
+    location = models.CharField(max_length=100, blank=True)
     prerequisites = models.ManyToManyField('self', symmetrical=False, blank=True)
-    units = models.IntegerField()
 
     def __str__(self):
         return f"{self.code} - {self.name}"
