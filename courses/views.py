f5260aef335f7ec400efdde113f52c91088f9212
BaharTaheripour <bahartp82@gmail.com>
2025-12-04 22:44:27 +0330

    feat: add IsAdminUser permission to CourseViewSet

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-#from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser
 from .models import Course
 from .serializers import CourseSerializer
 
@@ -7,4 +7,4 @@ from .serializers import CourseSerializer
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    #permission_classes = [IsAdminUser]
+    permission_classes = [IsAdminUser]
