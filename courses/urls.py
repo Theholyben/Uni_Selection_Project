ab80ba7d05da7da3cad0491d44bb28399b6a62f4
Benyamin Abdolseyedi <Bentako66@gmail.com>
2025-12-12 04:40:13 -0800

    make enrollments and my-courses endpoints

--- courses/urls.py
+++ courses/urls.py
@@ -3,7 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import CourseViewSet
 
 router = DefaultRouter()
-router.register('courses', CourseViewSet, basename='course')
+router.register(r'courses', CourseViewSet, basename='course')
+router.register(r'enrollments', EnrollmentViewSet)
+router.register(r'my-courses', ProfessorCourseViewSet, basename='my-courses')
 
 urlpatterns = [
     path('', include(router.urls)),
